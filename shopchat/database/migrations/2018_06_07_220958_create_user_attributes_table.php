6d23a4dd91b9d2c7f8dc820c85a70b5c69100fbf
Andrew Reinhardt <reinhardt786@gmail.com>
2018-06-07 18:43:15 -0400

    Built Message, Product, and UserAttribute models, with corresponding database migrations containing foreign key constraints

--- shopchat/database/migrations/2018_06_07_220958_create_user_attributes_table.php
+++ shopchat/database/migrations/2018_06_07_220958_create_user_attributes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateUserAttributesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('user_attributes', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('username');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->unsignedInteger('user_id');
+            $table->string('attr_name');
+            $table->string('attr_value');
             $table->timestamps();
+
+            // Foreign key constraints
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
@@ -31,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('user_attributes');
     }
 }
